6f406039a34f746dbb29dba32a56b2ea40d192ef
weibelzahl <pfh>
2016-07-13 09:41:54 +0200

    test with coersion

--- ActivityTracker/activityTracker.R
+++ ActivityTracker/activityTracker.R
@@ -1,4 +1,4 @@
-lesson_url <- paste("http://www.weibelzahl.de/swirl/R_Programmieren/", lesson, ".html", sep="")
+lesson_url <- paste("http://www.weibelzahl.de/swirl/R_Programmieren/", as.character(substitute(lesson), ".html", sep="")
 if (url.exists(lesson_url)){
 	getURL(lesson_url)
 	}
